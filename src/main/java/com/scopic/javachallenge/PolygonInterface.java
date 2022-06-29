7a6fbe63c1cedb3cde3756a1f2ead65b7ab2b8e9
Wenting <wenttzzheng@gmail.com>
2022-06-29 10:00:00 -0700

    Adds Poly interface

---
+++ src/main/java/com/scopic/javachallenge/PolygonInterface.java
@@ -0,0 +1,12 @@
+// /////////////////////////////////////////////////////////////////////////////
+// IMPORTANT:
+// THE CODE BELOW IS READ ONLY CODE AND YOU SHOULD INSPECT IT TO SEE WHAT IT
+// DOES IN ORDER TO COMPLETE THE TASK, BUT DO NOT MODIFY IT IN ANY WAY AS THAT
+// WILL RESULT IN A TEST FAILURE
+// /////////////////////////////////////////////////////////////////////////////
+
+package com.scopic.javachallenge;
+
+interface PolygonInterface {
+    public int getAngles();
+}
